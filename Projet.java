aa1d5792e7ed1fee6f6d5b02b6bb52bb1287dc57
omar <omardiop2@esp.sn>
2024-05-09 23:04:08 +0000

    second commit

--- Projet.java
+++ Projet.java
@@ -1,10 +1,40 @@
 import java.math.BigInteger;
 import java.security.MessageDigest;
+import java.util.HashSet;
 import java.util.List;
 public class Projet{
 
+    public static void main(String[] args) {
+        // Exemple de mots de passe à tester
+        String password1 = "omar";
+        String password2 = "hello";
+        String password3 = "123456";
+
+        // Créer des hash MD5 de ces mots de passe
+        String hash1 = md5(password1);
+        String hash2 = md5(password2);
+        String hash3 = md5(password3);
+
+        // Afficher les hash pour vérification
+        System.out.println("Hash 1: " + hash1);
+        System.out.println("Hash 2: " + hash2);
+        System.out.println("Hash 3: " + hash3);
+
+        // Créer une instance de BruteforceLocalCracker et tester
+        BruteforceLocalCracker bruteforcer = new BruteforceLocalCracker();
+        System.out.println("Testing Bruteforce Cracker:");
+        bruteforcer.crackPassword(hash1);  // Devrait réussir pour des mots courts avec force brute
+
+        // Créer une instance de DictionaryLocalCracker avec un petit dictionnaire
+        List<String> dictionary = List.of("hello", "world", "password", "123456", "admin");
+        DictionaryLocalCracker dictionaryCracker = new DictionaryLocalCracker(dictionary);
+        System.out.println("Testing Dictionary Cracker:");
+        dictionaryCracker.crackPassword(hash2);  // Devrait réussir avec une attaque de dictionnaire
+        dictionaryCracker.crackPassword(hash3);  // Devrait aussi réussir
+    }
+
     
-public abstract class PasswordCrackerFactory {
+public static abstract class PasswordCrackerFactory {
   public abstract PasswordCracker createPasswordCracker();
     
 }
@@ -34,7 +64,7 @@ public static String md5(String input) {
 }
 
 
-public abstract class LocalCracker extends PasswordCrackerFactory {
+public static abstract class LocalCracker extends PasswordCrackerFactory {
     
     public PasswordCracker createPasswordCracker(){
         return (PasswordCracker) this;
@@ -42,7 +72,7 @@ public abstract class LocalCracker extends PasswordCrackerFactory {
 }
 
 
-public class BruteforceLocalCracker extends LocalCracker {
+public static class BruteforceLocalCracker extends LocalCracker {
 
    
       public void  crackPassword(String hashedpassword){
@@ -82,34 +112,56 @@ public class BruteforceLocalCracker extends LocalCracker {
 }
 
 
-public class DictionaryLocalCracker extends LocalCracker {
+public static class DictionaryLocalCracker extends LocalCracker {
+
+    private List<String> dictionary;
+
+        public DictionaryLocalCracker(List<String> dictionary) {
+            this.dictionary = dictionary;
+        }
+
+        public List<String> getDictionary() {
+            return dictionary;
+        }
 
     public void crackPassword(String hashedpassword){
         dictionaryAttack(hashedpassword);
         
     }
 
-    public DictionaryLocalCracker(List<String> dictionary) {
-    }
+    
 
                                                      
     
 
-    public static void dictionaryAttack(String hashedPassword) {
-        System.out.println("Cracking password using dictionary...");
-        // Vous pouvez fournir ici une liste de mots de passe à partir d'un fichier ou d'une source externe
-        List<String> dictionary = List.of("password", "qwerty", "123456", "letmein", "admin");
-
-        for (String word : dictionary) {
-            // Calculer le hash MD5 du mot de passe du dictionnaire
-            String hashedWord = md5(word);
-            if (hashedWord.equals(hashedPassword)) {
-                System.out.println("Password cracked using dictionary: " + word);
-                return;
-            }
+    public  void dictionaryAttack(String hashedPassword) {
+        if (dictionary == null) {
+            System.out.println("Default dictionary is not set.");
+            return;
         }
-        System.out.println("Password not found in dictionary.");
+
+        dictionaryAttack(hashedPassword, dictionary);
     }
+
+      public void dictionaryAttack(String hashedPassword, List<String> customDictionary) {
+             System.out.println("Cracking password using dictionary...");
+    
+            // Précalculez les hachages du dictionnaire
+            HashSet<String> hashedDictionary = new HashSet<>();
+            for (String word : customDictionary) {
+                hashedDictionary.add(md5(word));
+            }
+            
+            // Recherche dans le dictionnaire
+            for (String hashedWord : hashedDictionary) {
+                if (hashedWord.equals(hashedPassword)) {
+                    System.out.println("Password cracked using dictionary.");
+                    return;
+                }
+            }
+            
+            System.out.println("Password not found in dictionary.");
+        }
 
 }
 
